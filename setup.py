43fe7408eb8b27664c28a80e0aea352a853f7f14
gaurav61 <gaurav.arora0077@gmail.com>
2021-03-28 21:13:47 +0530

    bug fix in main_py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 setup(
     name = 'filezilla',
-    version = '1.0',
+    version = '1.2',
     scripts = ['./script/filezilla'],
     author = 'Gaurav Arora',
     description = ('File manager'),
+    long_description = README,
+    long_description_content_type = "text/markdown",
     packages = ['filezilla'],
     install_requires=[
         'setuptools',
